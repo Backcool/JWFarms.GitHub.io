62a5dfe9d6460ced60423e4b1c680652801590d6
Angel Martinez <backcool@Angels-MacBook-Pro.local>
2024-07-08 15:56:52 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms'; 
 import { Routes } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { FooterComponent } from "./footer/footer.component";
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,MatToolbarModule,MatFormFieldModule,FormsModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, MatToolbarModule, MatFormFieldModule, FormsModule, MatSidenavModule, FooterComponent]
 })
 export class AppComponent {
   title = 'business-webpage';
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSidenavModule} from '@angular/material/sidenav';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatToolbarModule, MatFormFieldModule],
+  imports: [MatToolbarModule, MatFormFieldModule, MatSidenavModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
